8c6f502f7a3ffcc3373e960f9a69ee568ca9c910
Chmox <chchema26@gmail.com>
2023-09-21 22:43:17 -0600

    Added blog and services app

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path 
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('about-me/', views.about, name="about"),
+    path('visit/', views.visit, name="visit"),
+    path('contact/', views.contact, name="contact"),
+    path('blog/', views.blog, name="blog"),
+    path('sample/', views.sample, name="sample"),
+]
